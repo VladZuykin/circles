4c9dc5d763794a2b2843cf57bf9874b96d71f514
vladislav <vladpzuykinvc@gmail.com>
2021-01-09 16:47:50 +0500

    Рисуются окружности случайного радиуса и цвета.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
 from random import randrange
+from Ui import Ui_Form
+
+def generate_color():
+    return randrange(0, 255), randrange(0, 255), randrange(0, 255)
 
 
-class CircleDrawer(QWidget):
+class CircleDrawer(QWidget, Ui_Form):
     CIRCLES_NUMBER = 5
     BORDER = 5
 
     def __init__(self):
         super().__init__()
-        uic.loadUi("Ui.ui", self)
+        self.setupUi(self)
         self.ellipses = []
         self.need_to_draw = False
         self.drawButton.clicked.connect(self.draw)
@@ -27,21 +30,23 @@ class CircleDrawer(QWidget):
         qp.end()
 
     def draw_circles(self, qp):
-        qp.setBrush(QColor('yellow'))
         width, height = self.width(), self.height()
         if self.need_to_draw:
             self.draw_last_circles(qp)
+            color = generate_color()
+            qp.setBrush(QColor(*color))
             radius = randrange(5, 60)
             x, y = randrange(radius + self.BORDER, width - radius * 2 - self.BORDER), \
                 randrange(radius + self.BORDER, height - radius * 2 - self.BORDER)
             qp.drawEllipse(x, y, radius * 2, radius * 2)
-            self.ellipses.append((x, y, radius))
+            self.ellipses.append((x, y, radius, color))
             self.need_to_draw = False
         else:
             self.draw_last_circles(qp)
 
     def draw_last_circles(self, qp):
-        for x, y, radius in self.ellipses:
+        for x, y, radius, color in self.ellipses:
+            qp.setBrush(QColor(*color))
             qp.drawEllipse(x, y, radius * 2, radius * 2)
 
 
